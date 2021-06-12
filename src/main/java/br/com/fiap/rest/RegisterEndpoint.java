e6abbe5548bb5fd5c8f7fbae0cc4a31e129e4d0b
allanpreis31@gmail.com <allanpreis31@gmail.com>
2021-06-11 21:52:28 -0300

    Finalização do rest

--- src/main/java/br/com/fiap/rest/RegisterEndpoint.java
+++ src/main/java/br/com/fiap/rest/RegisterEndpoint.java
@@ -11,82 +11,64 @@ import java.util.List;
 @Path("registers")
 @Produces(MediaType.APPLICATION_JSON)
 public class RegisterEndpoint {
-	
-	RegisterDAO dao = new RegisterDAO();
-	
+
+	private RegisterDAO dao = new RegisterDAO();
+
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Register> index() {
 		return dao.getAll();
 	}
-	
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response create(Register register) {
 		if (register == null) {
-			return Response
-					.status(Response.Status.BAD_REQUEST)
-					.build();
+			return Response.status(
+					Response.Status.BAD_REQUEST).build();
 		}
 		try {
 			dao.save(register);
 		} catch (Exception e) {
-			return Response
-					.status(Response.Status.INTERNAL_SERVER_ERROR)
-					.build();
+			return Response.status(
+					Response.Status.INTERNAL_SERVER_ERROR).build();
 		}
-		return Response
-				.status(Response.Status.CREATED)
-				.entity(register)
-				.build();
+		return Response.status(
+				Response.Status.CREATED).entity(register).build();
 	}
-	
+
 	@GET
 	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response show(@PathParam("id") Long id) {
 		Register register = dao.findById(id);
 		if (register == null) {
-			return Response
-					.status(Response.Status.NOT_FOUND)
-					.build();
-
+			return Response.status(
+					Response.Status.BAD_REQUEST).build();
 		}
-		return Response
-					.status(Response.Status.OK)
-					.entity(register)
-					.build();
+		return Response.status(Response.Status.OK).entity(register).build();
 	}
-	
+
 	@PUT
 	@Path("{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response update(@PathParam("id") Long id, Register register ) {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response update(@PathParam("id") Long id, Register register) {
+
+		if (dao.findById(id) == null) {
+			return Response.status(
+					Response.Status.BAD_REQUEST).build();
+		}
+		if (register == null) {
+			return Response.status(
+					Response.Status.BAD_REQUEST).build();
+		}
 		register.setId(id);
 		dao.update(register);
-		return Response
-				.status(Response.Status.OK)
-				.entity(register)
-				.build();
-	}
 
-    @DELETE
-	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response delete(@PathParam("id") Long id, Register register) {
-      register.getId(id);
-      if (register == null) {
-		  return Response.status(Response.Status.NOT_FOUND).build();
-	  }
-      try {
-      	dao.delete(id);
-      	return Response
-				.status(Response.Status.OK)
-				.build();
-	  } catch (Exception ex) {
-      	return Response
-				.status(Response.Status.INTERNAL_SERVER_ERROR)
-				.entity(ex.getMessage())
-				.build();
-	  }
-	}
 
+		return Response.status(Response.Status.OK).entity(register).build();
+
+	}
 }
